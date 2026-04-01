b530abdf8b371f408e26e1d1cd77ccf65423353e
patrick-j-sulley <dxhhhshawn@gmail.com>
2026-04-01 15:04:54 +1300

    Define project views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Project
+from .serializers import ProjectSerializer
 
 # Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_permissions(self):
+        """
+        Public can list/retrieve.
+        Only authenticated users can create/update/delete.
+        """
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
