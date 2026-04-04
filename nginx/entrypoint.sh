0a49a961aa923528adf4d2a08a72defe630bfd45
patrick-j-sulley <dxhhhshawn@gmail.com>
2026-04-05 01:36:56 +1300

    Fix envsubst to properly export environment variables

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+set -e
+
+echo "BACKEND_HOST=${BACKEND_HOST}"
+echo "BACKEND_PORT=${BACKEND_PORT}"
+echo "FRONTEND_HOST=${FRONTEND_HOST}"
+echo "FRONTEND_PORT=${FRONTEND_PORT}"
+
+export BACKEND_HOST
+export BACKEND_PORT
+export FRONTEND_HOST
+export FRONTEND_PORT
+
 envsubst '${BACKEND_HOST},${BACKEND_PORT},${FRONTEND_HOST},${FRONTEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "Generated config:"
+cat /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
