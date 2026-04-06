ae891baa3111f9e798ee28c6cc8d9559852800da
patrick-j-sulley <dxhhhshawn@gmail.com>
2026-04-06 18:50:31 +1200

    Updated nginx port handling

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,9 +6,11 @@ set -e
 : "${FRONTEND_HOST:?FRONTEND_HOST is required}"
 : "${FRONTEND_PORT:?FRONTEND_PORT is required}"
 
-export BACKEND_HOST BACKEND_PORT FRONTEND_HOST FRONTEND_PORT
+# Railway sets PORT for the public HTTP listener; default 80 for local docker-compose (8080:80 mapping).
+PORT="${PORT:-80}"
+export BACKEND_HOST BACKEND_PORT FRONTEND_HOST FRONTEND_PORT PORT
 
-envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT}' \
+envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT} ${PORT}' \
   < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
